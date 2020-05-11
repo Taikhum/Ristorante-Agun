e2512d095fe543a3f0795cd8c8c3e24e2c725f44
taikhum <taikhumk@gmail.com>
2020-05-11 16:07:07 +0530

    Single page aplications 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -8,5 +8,6 @@ import { MenuComponent } from '../menu/menu.component';
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'menu', component: MenuComponent},
-  {path: '', redirectTo:'/home', pathMatch:'full'}
+  { path: 'contactus', component: ContactComponent },
+  {path: '', redirectTo:'/home', pathMatch:'full'},
 ];
